40fac27b68d113457ccda466c15224a3c3cd5545
jen-sei <101796507+jen-sei@users.noreply.github.com>
2024-07-14 08:45:40 +0200

    v2

--- src/app/providers/ConnectWallet.tsx
+++
@@ -1,9 +0,0 @@
-import { WagmiProvider, useAccount } from "wagmi";
-import { Account } from "./Account";
-import { WalletOptions } from "./WalletOptions";
-
-export default function ConnectWallet() {
-  const { isConnected } = useAccount();
-  if (isConnected) return <Account />;
-  return <WalletOptions />;
-}
